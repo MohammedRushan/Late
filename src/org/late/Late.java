a519c9d644991269c701dbea9a8cd408c558204f
Muhajir M.R <mohamemr@amazon.com>
2021-05-24 11:18:51 -0700

    late

---
+++ src/org/late/Late.java
@@ -0,0 +1,15 @@
+package org.late;
+
+public class Late {
+public static void main(String[] args) {
+	
+	int a;
+	for (int i = 0; i < args.length; i++) {
+		i++;
+	}
+	
+	
+	System.out.println("predefined");
+	System.out.println("predefined");
+}
+}
